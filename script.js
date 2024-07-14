42dfe9cf7dae5f6041ceba959c879992eb9b3413
MrVooDooNFT <122112409+MrVooDooNFT@users.noreply.github.com>
2024-07-14 18:04:34 +0300

    script.js

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function calculatePoints() {
     const diskCapacity = parseFloat(document.getElementById('diskCapacity').value);
 
     const basePoints = ramMultiplier * cpuMultiplier * internetSpeedMultiplier * (diskCapacity / 10) * diskTypeMultiplier;
-    const cappedBasePoints = basePoints > 450 ? 450 : basePoints;
     const pointsFirst24Hours = basePoints > 450 ? 450 * 0.8 : basePoints * 0.8;
-    const pointsAfter24Hours = cappedBasePoints;
+    const cappedBasePoints = basePoints > 450 ? 450 : basePoints;
+    const pointsAfter24Hours = cappedBasePoints * 1.25;
 
     const resultMessage = basePoints > 450 ? 
         `Due to the maximum point limit, your points will be capped at 450.` :
